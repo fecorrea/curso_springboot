75e3015fee26f38e09eb1e05c6acbb8caf07de16
Fressia Correa <fressiacorreaf@gmail.com>
2020-07-08 11:22:11 -0500

    cambio en index de navbar por inicio y en el controller la ruta y sout

--- cursopringboot/src/main/java/com/example/cursopringboot/controllers/ControllerBasic.java
+++ cursopringboot/src/main/java/com/example/cursopringboot/controllers/ControllerBasic.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hola")
+@RequestMapping("/")
 public class ControllerBasic {
     @GetMapping(path ={"/saludar", "/holamundo"})
     public String saludar(Model model){
     	model.addAttribute("name", "el modelo");
+    	System.out.println("hdsfjkh cxjhefid");
         return "index";
     }
 }
